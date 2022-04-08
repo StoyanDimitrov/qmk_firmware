143ec3d23e8edd7fc8495b3966ffd3332d12a8de
Harshit Goel--HOME <harshitgoel96@yahoo.com>
2022-04-09 00:14:33 +0530

    added working keyboard

--- keyboards/mano42/config.h
+++ keyboards/mano42/config.h
@@ -3,18 +3,48 @@
 
 #pragma once
 
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID    0x4847 //HG
+#define PRODUCT_ID   0x4D4E //MN
+#define DEVICE_VER   0x0001
+#define MANUFACTURER Harshit Goel
+#define PRODUCT      Mano42
+
+/* key matrix size */
+#define MATRIX_ROWS 10
+#define MATRIX_COLS 5
+
 /*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
+ * Keyboard Matrix Assignments
+ *
+ * Change this to how you wired your keyboard
+ * COLS: AVR pins used for columns, left to right
+ * ROWS: AVR pins used for rows, top to bottom
+ * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
+ *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
+ *
  */
+#define MATRIX_ROW_PINS { C6, D7, E6, B5, B4 }
+#define MATRIX_COL_PINS { F6, F7, B1, B3,  B2 }
+#define UNUSED_PINS
 
-/* disable debug print */
-//#define NO_DEBUG
+/* COL2ROW, ROW2COL */
+#define DIODE_DIRECTION ROW2COL
 
-/* disable print */
-//#define NO_PRINT
+/*
+ * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
+ */
+#define USE_SERIAL
+#define SOFT_SERIAL_PIN D3  // or D1, D2, D3, E6
 
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 5
+
+//qmk-dfu
+#define QMK_ESC_OUTPUT C6  // COL pin if COL2ROW
+#define QMK_ESC_INPUT  F6  // ROW pin if COL2ROW
+
+// for via
+//#define DYNAMIC_KEYMAP_LAYER_COUNT 7
--- keyboards/mano42/rules.mk
+++ keyboards/mano42/rules.mk
@@ -1,1 +1,23 @@
-# This file intentionally left blank
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = qmk-dfu
+
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = no            # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
+OLED_ENABLE = no           # OLED display
+ENCODER_ENABLE = no        # Encoder support
+SPLIT_KEYBOARD = yes        # Split enable
+
+#LAYOUTS = split_3x5_3
