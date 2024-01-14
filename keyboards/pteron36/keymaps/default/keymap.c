79f4130a3b7b2567372ccdcf5f73860441789ef2
Stoyan Dimitrov <stoyan@gmx.com>
2024-01-14 21:32:31 +0200

    Add VIA support

--- keyboards/pteron36/keymaps/default/keymap.c
+++ keyboards/pteron36/keymaps/default/keymap.c
@@ -11,7 +11,7 @@
   * 
   * You should have received a copy of the GNU General Public License 
   * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
-  */ 
+  */
 #include QMK_KEYBOARD_H
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
